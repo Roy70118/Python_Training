818a03345ab591ee32e0666a81b639f390f36dd7
Roy70118 <mayankkumar27sept.mk@gmail.com>
2025-03-22 11:02:57 +0530

    pushing python files

---
+++ session-1/first.py
@@ -0,0 +1,114 @@
+# print("hello world")
+
+# script mode
+# command prompt
+
+# Interactive mode
+# vs code
+
+# print("MAYANK")
+# print("Akash")
+# ctrl+slash---> comment out
+
+
+# list = ["hello world", "mayank", "akash"]
+# for i in range(len(list)):
+#     print(list[i])
+
+
+# Inbuilt function(Build in module)
+# import random
+
+# print(random.randint(1, 10))
+
+# """
+# pip is  a package manager , used to install
+# external modules
+
+# pip install Django
+# pip install Flask
+
+# REPL--> READ Evaluate print Loop
+
+# myank
+# """
+
+# Identation:
+
+# space at the beginning of the code
+
+# identation is very important in python
+# four spaces used in python to indicate a block of code
+
+# if 5 > 2:
+#     print("5 is greater than 2")
+# (:) colon
+
+# var = 2
+# if var % 3 == 0:
+#     print("True")
+# else:
+#     print("pushkar")
+
+
+# variables
+
+# variables r created when you assign a value to it
+
+# varsha = "hodage"
+# y = 5
+# x=3
+# d="mayank"
+
+
+# print(varsha, y,x,d)
+
+# type() function
+
+# data types
+# primtive dataypes
+
+# integer
+# string
+# boolean
+# float -- 2.5
+
+# import datetime
+# # built in module
+
+# x = datetime.datetime.now()
+# print(x)
+
+
+# x = "mayank"
+# print(type(x))
+
+# x = 9
+# print(type(x))
+
+
+# y = True
+# print(type(y))
+
+
+# z = 3.55
+# print(type(z))
+
+
+# overwrite
+# x = "mayank"
+# x = "varsha"
+# x = "pushkar"
+# print(x)
+
+
+# python is a case-sensitive language
+X = "Mayank"
+x = "varsha"
+# print(x, X)
+print(x)
+print(X)
+
+
+for i in range(10):
+    print(i, end=" ")
